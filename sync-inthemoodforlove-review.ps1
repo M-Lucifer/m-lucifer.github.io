0da5e6f91c9623733fb9d964aafb0cb3c25ef87f
M-Lucifer <118010300@link.cuhk.edu.cn>
2026-03-31 20:32:53 +0800

    Fix inthemoodforlove review sync encoding

--- sync-inthemoodforlove-review.ps1
+++ sync-inthemoodforlove-review.ps1
@@ -5,6 +5,7 @@ $sourceRepo = 'G:\pretext-examples'
 $targetDir = Join-Path $siteRepo 'inthemoodforlove-review'
 $assetsDir = Join-Path $targetDir 'assets'
 $vendorDir = Join-Path $targetDir 'vendor\pretext'
+$utf8NoBom = New-Object System.Text.UTF8Encoding($false)
 
 if (-not (Test-Path $siteRepo)) {
   throw "Site repo not found: $siteRepo"
@@ -27,13 +28,13 @@ $distSource = Join-Path $sourceRepo 'node_modules\@chenglou\pretext\dist'
 Copy-Item (Join-Path $distSource '*.js') $vendorDir -Force
 
 $indexPath = Join-Path $targetDir 'index.html'
-$indexContent = Get-Content $indexPath -Raw
+$indexContent = [System.IO.File]::ReadAllText($indexPath, [System.Text.Encoding]::UTF8)
 $indexContent = $indexContent.Replace('<a class="back" href="../index.html">Back to index</a>', '<a class="back" href="/">Home</a>')
 $indexContent = $indexContent.Replace('<script type="module" src="./07-inthemoodforlove-review.js"></script>', '<script type="module" src="./app.js"></script>')
-Set-Content $indexPath $indexContent
+[System.IO.File]::WriteAllText($indexPath, $indexContent, $utf8NoBom)
 
 $appPath = Join-Path $targetDir 'app.js'
-$appContent = Get-Content $appPath -Raw
+$appContent = [System.IO.File]::ReadAllText($appPath, [System.Text.Encoding]::UTF8)
 $appContent = $appContent.Replace("from '../node_modules/@chenglou/pretext/dist/layout.js'", "from './vendor/pretext/layout.js'")
 $appContent = $appContent.Replace("const ASSET_ROOT = '../inthemoodforlove'", "const ASSET_ROOT = './assets'")
-Set-Content $appPath $appContent
+[System.IO.File]::WriteAllText($appPath, $appContent, $utf8NoBom)
