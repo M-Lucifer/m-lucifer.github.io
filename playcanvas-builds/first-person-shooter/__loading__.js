a473410682548e74ec6d8463df510a48138240fe
M-Lucifer <118010300@link.cuhk.edu.cn>
2026-05-18 11:37:45 +0800

    Add PlayCanvas first-person shooter demo

---
+++ playcanvas-builds/first-person-shooter/__loading__.js
@@ -0,0 +1,100 @@
+pc.script.createLoadingScreen((app) => {
+    const createCss = () => {
+        const css = `
+            body {
+                background-color: #283538;
+            }
+
+            #application-splash-wrapper {
+                position: absolute;
+                top: 0;
+                left: 0;
+                height: 100%;
+                width: 100%;
+                background-color: #283538;
+            }
+
+            #application-splash {
+                position: absolute;
+                top: calc(50% - 28px);
+                width: 264px;
+                left: calc(50% - 132px);
+            }
+
+            #application-splash img {
+                width: 100%;
+            }
+
+            #progress-bar-container {
+                margin: 20px auto 0 auto;
+                height: 2px;
+                width: 100%;
+                background-color: #1d292c;
+            }
+
+            #progress-bar {
+                width: 0%;
+                height: 100%;
+                background-color: #f60;
+            }
+
+            @media (max-width: 480px) {
+                #application-splash {
+                    width: 170px;
+                    left: calc(50% - 85px);
+                }
+            }
+        `;
+
+        const style = document.createElement('style');
+        style.textContent = css;
+        document.head.appendChild(style);
+    };
+
+    const showSplash = () => {
+        const wrapper = document.createElement('div');
+        wrapper.id = 'application-splash-wrapper';
+        document.body.appendChild(wrapper);
+
+        const splash = document.createElement('div');
+        splash.id = 'application-splash';
+        wrapper.appendChild(splash);
+        splash.style.display = 'none';
+
+        const logo = document.createElement('img');
+        logo.src = `${ASSET_PREFIX}logo.png`;
+        splash.appendChild(logo);
+        logo.onload = () => {
+            splash.style.display = 'block';
+        };
+
+        const container = document.createElement('div');
+        container.id = 'progress-bar-container';
+        splash.appendChild(container);
+
+        const bar = document.createElement('div');
+        bar.id = 'progress-bar';
+        container.appendChild(bar);
+    };
+
+    const setProgress = (value) => {
+        const bar = document.getElementById('progress-bar');
+        if (bar) {
+            value = Math.min(1, Math.max(0, value));
+            bar.style.width = `${value * 100}%`;
+        }
+    };
+
+    const hideSplash = () => {
+        document.getElementById('application-splash-wrapper').remove();
+    };
+
+    createCss();
+    showSplash();
+
+    app.on('preload:end', () => {
+        app.off('preload:progress');
+    });
+    app.on('preload:progress', setProgress);
+    app.on('start', hideSplash);
+});
---
+++ playcanvas-builds/first-person-shooter/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2501707.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 290833663, 290833372, 290833375, 290833380, 290833362 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/290833367/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/290833366/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/290833365/1/ammo.js', 'preload' : true},
+];
